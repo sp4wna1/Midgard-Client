34ac015165670fc8bada2425519b948522700131
Gabriel Prado <gabriel.prado@bloov.com>
2019-10-15 23:04:40 -0300

    Fixing disconnect problem

--- modules/gamelib/game.lua
+++ modules/gamelib/game.lua
@@ -33,8 +33,8 @@ function g_game.chooseRsa(host)
     g_game.setRsa(OTSERV_RSA)
   end
 
-  -- Hack fix to resolve some 760 login issues
-  if g_game.getClientVersion() <= 760 then
+  -- Hack fix to resolve some 792 login issues
+  if g_game.getClientVersion() <= 792 then
     g_game.setCustomOs(2)
   end
 end
